b87b6761cede16f684866b9a06ac42c0c659120a
tomas zelante <103254329+tomaszelante@users.noreply.github.com>
2022-09-13 14:58:31 -0300

    Update ej1_tp51.sh

--- tp5.1/ej1_tp51.sh
+++ tp5.1/ej1_tp51.sh
@@ -3,5 +3,5 @@ echo ingrese su primer numero
 read num1
 echo ingrese su segundo numero
 read num2
-$(result=num1-num2)
+$((result=num1-num2))
 echo el resultado entre la resta de los numeros es de $result
